13d798168a1979706c3897e60a8e102a687cb520
Bruno Modinuti <modinutidev@gmail.com>
2019-08-09 21:48:07 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function send(){
+  number=$('input').val();
+  if(number==''){
+    alert('Entre com um número de celular para continuar!');
+  }else{
+    url='https://api.whatsapp.com/send?phone='+number+'&text=';
+    $('input').val('');
+    window.open(url, '_blank');
+  }
+}
+
+$('button').click(send);
+
+$("input").keypress(function(e){//Adiciona ação de actionkey no input
+  if(e.which == 13) {
+   send();
+  }
+});
