d2c694f7a8ec1f90f8bc555eb24e356292bae777
Bruno Modinuti <modinutidev@gmail.com>
2019-08-09 23:08:54 -0300

    Adicionado script do google analytics

--- index.php
+++ index.php
@@ -20,12 +20,11 @@
     <!-- icon -->
     <link rel="icon" href="img/icone_whatsapp.png" type="image/x-icon" />
     <link rel="shortcut icon" href="img/icone_whatsapp.png" type="image/x-icon" />
-    <!-- bootstrap -->
+    <!-- styles -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-    <!-- estilização -->
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
@@ -36,23 +35,31 @@
                 <div class="form-group">
                     <label>Número</label>
                     <input class="form-control form-control" type="tel" placeholder="Ex.5511999999999">
-                </div><!-- form-group -->
-            </div><!-- col -->
+                </div>
+            </div>
             <div class="col-sm-12">
                 <div class="form-group">
                     <button class="btn btn-success">Criar Conversa</button>
-                </div><!-- form-group -->
-            </div><!-- col -->
-        </div><!-- row -->
+                </div>
+            </div>
+        </div>
         <div class="row">
             <div class="col-6">
                 <a href="https://google.com.br" target="_blank">Como usar?</a>
-            </div><!-- col -->
+            </div>
             <div class="col-6 right">
                 <a href="https://instagram.com/brunomodinuti" target="_blank">Bruno Modinuti</a>
-            </div><!-- col -->
-        </div><!-- row -->
-    </div><!-- container -->
+            </div>
+        </div>
+    </div>
 </body>
+<!-- scripts -->
 <script  src="./js/script.js"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-145407135-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-145407135-1');
+</script>
 </html>
